4cc68f1e60941f311998b545a70b24250c001dff
Kenny Cheng <kennycheng@hkbu.edu.hk>
2023-09-04 01:27:53 +0800

    exclude dev sys and proc in tar

--- fetch.sh
+++ fetch.sh
@@ -4,4 +4,4 @@ GENTOO_MIRROR="http://gentoo.aditsu.net"
 
 wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-openrc.txt | grep tar | awk '{print $1}')"
 mkdir stage3
-tar xvJf tarball.tar.xz -C stage3
+tar xvJf tarball.tar.xz -C stage3 --exclude ./dev/* --exclude ./sys/* --exclude ./proc/*
