c871a9fe3ceeae74c3119cb978360ee592771039
Kenny Cheng <kennycheng@hkbu.edu.hk>
2023-09-05 16:01:24 +0800

    add stage4 dockerfile

--- fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-GENTOO_MIRROR="http://gentoo.aditsu.net"
-
-wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-openrc.txt | grep tar | awk '{print $1}')"
-mkdir stage3
-tar xvJf tarball.tar.xz -C stage3 --exclude ./dev* --exclude ./sys* --exclude ./proc* --exclude ./run* --exclude ./var/run*
