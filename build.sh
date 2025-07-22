278543ff63a1d17a5a82e08b0c5305cef31a5252
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2025-07-22 18:43:57 +0800

    update mirror and image tag

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-DOCKER_TAG="kennycheng/gentoo-stage3:x32"
-GENTOO_MIRROR="http://gentoo.aditsu.net"
+DOCKER_TAG="kennycheng/gentoo-stage3:x32-openrc"
+GENTOO_MIRROR="https://distfiles.gentoo.org"
 
 wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-openrc.txt | grep tar | awk '{print $1}')"
 mkdir stage3
--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GENTOO_MIRROR="http://gentoo.aditsu.net"
+GENTOO_MIRROR="https://distfiles.gentoo.org"
 
 wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-openrc.txt | grep tar | awk '{print $1}')"
 mkdir stage3
