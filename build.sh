9695c860e1106ea5e197c7b1392f5fbfab5af7fc
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2025-07-22 18:47:24 +0800

    modify for systemd tarball

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-DOCKER_TAG="kennycheng/gentoo-stage3:x32-openrc"
+DOCKER_TAG="kennycheng/gentoo-stage3:x32-systemd"
 GENTOO_MIRROR="https://distfiles.gentoo.org"
 
-wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-openrc.txt | grep tar | awk '{print $1}')"
+wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-systemd.txt | grep tar | awk '{print $1}')"
 mkdir stage3
 tar xvJf tarball.tar.xz -C stage3
 docker build -t ${DOCKER_TAG} .
--- fetch.sh
+++ fetch.sh
@@ -2,6 +2,6 @@
 
 GENTOO_MIRROR="https://distfiles.gentoo.org"
 
-wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-openrc.txt | grep tar | awk '{print $1}')"
+wget -O tarball.tar.xz "${GENTOO_MIRROR}/releases/amd64/autobuilds/$(wget -O - ${GENTOO_MIRROR}/releases/amd64/autobuilds/latest-stage3-x32-systemd.txt | grep tar | awk '{print $1}')"
 mkdir stage3
 tar xvJf tarball.tar.xz -C stage3 --exclude ./dev* --exclude ./sys* --exclude ./proc* --exclude ./run* --exclude ./var/run*
